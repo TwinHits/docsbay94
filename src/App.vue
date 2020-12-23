9ca3e2169195b4540f737df68d6ff1d52ca05a0b
John Goben <TwinHits@gmail.com>
2020-12-22 23:44:26 -0500

    Removed deprecated v-content.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
     <v-app>
-        <v-content>
-            <v-main>
-                <v-container id="app">
-                    <router-view />
-                </v-container>
-            </v-main>
-        </v-content>
+        <v-main>
+            <v-container id="app">
+                <router-view />
+            </v-container>
+        </v-main>
     </v-app>
 </template>
 
 <style lang="scss">
 @import '@/styles/colors.scss';
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     background-color: $background-black;
