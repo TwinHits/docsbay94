912bac4cf6355b264eb403c4bb3d5691b84e8289
TwinHits <TwinHits@gmail.com>
2020-12-22 22:33:37 -0500

    Initial set up of Vue, vuetify, scss, and boilerplate cleanup.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+    <v-app>
+        <v-content>
+            <v-main>
+                <v-container id="app">
+                    <router-view />
+                </v-container>
+            </v-main>
+        </v-content>
+    </v-app>
 </template>
 
 <style lang="scss">
+@import "@/styles/colors.scss";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    background-color: $background-black;
+    width: 100%;
+    height: 100%;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+    margin: 0;
+    width: 100%;
+    height: 100%;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-page">
+      <v-img class="tie-logo" contain :src="logoUrl" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    data() {
+        return {
+            logoUrl: "https://raw.githubusercontent.com/TwinHits/TIEngine/master/assets/textures/logo.png" as string,
+        }
+    }
+});
+</script>
+<style lang="scss">
+
+.tie-logo {
+    width: 25vw;
+}
+
+</style>
