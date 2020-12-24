b68410526ae49ce6173b365c78687f40201b249d
John Goben <TwinHits@gmail.com>
2020-12-24 01:17:09 -0500

    Wrote common IconButton component.

---
+++ src/components/common/IconButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-icon class="icon-button" :color="iconColor" :size="size" :disabled="disabled" @click="handleClick">
+        {{ icon }}
+    </v-icon>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import * as Constants from '@/common/constants';
+
+export default Vue.extend({
+    props: {
+        icon: {
+            type: String,
+            required: true,
+        },
+        size: {
+            type: Number,
+            required: false,
+            default: 40,
+        },
+        disabled: {
+            type: Boolean,
+            required: false,
+            default: false,
+        }
+    },
+    data() {
+        return {
+            iconColor: Constants.COLORS.BACKGROUND_BLACK,
+        };
+    },
+    methods: {
+        handleClick() {
+            this.$emit("click");
+        }
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
